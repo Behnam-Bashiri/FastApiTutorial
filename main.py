2c63e2f47118c73f51074320b8110eb4fbfa2a64
Behnam-Bashiri <behnambashiri80@gmail.com>
2023-09-23 18:55:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from re import U
 from tkinter import N
 from typing import Union
-
-from fastapi import FastAPI,Query
+from pydantic import BaseModel, EmailStr
+from fastapi import FastAPI,Query,Form,File,UploadFile
 
 app = FastAPI()
 
+@app.post("/files/")
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
 
-@app.get("/items/")
-async def read_items(q: Union[str,None]=Query(default=None, max_length=50)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
